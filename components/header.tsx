4017aaf61e79351e1397e026143d63cb8c37c6c3
tricky <trickya23@gmail.com>
2024-05-15 06:06:42 +0000

    fix bug

--- components/header.tsx
+++ components/header.tsx
@@ -1,44 +1,45 @@
 "use client";
 
+import { Transition } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { useState } from "react";
 import { HeaderLogo } from "./headerLogo";
 import { NavLinks } from "./navLinks";
-import { useState } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { Transition } from "@headlessui/react";
+import clsx from "clsx";
 
 const menuLinks = [
   {
     id: 1,
     name: "Home",
-    link: "/"
+    link: "/",
   },
   {
     id: 2,
     name: "About",
-    link: "/about"
+    link: "/about",
   },
   {
     id: 3,
     name: "Get Involved",
-    link: "/get-involved"
+    link: "/get-involved",
   },
   {
     id: 4,
     name: "Projects",
-    link: "/projects"
+    link: "/projects",
   },
   {
     id: 5,
     name: "Blog",
-    link: "/blog"
+    link: "/blog",
   },
   {
     id: 6,
     name: "Contact",
-    link: "/contact"
-  }
-]
+    link: "/contact",
+  },
+];
 
 export function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -76,13 +77,14 @@ export function Header() {
         leave="transition ease-in-out duration-300"
         leaveFrom="translate-x-0"
         leaveTo="-translate-x-full"
-        className="fixed h-screen w-[70%] text-slate-300 bg-[#491BA2] top-0 left-0 ease-in-out duration-200 md:w-[40%]"
       >
         <nav className="flex flex-col gap-9 text-xl items-center justify-evenly md:text-2xl md:font-semibold md:gap-12">
           {menuLinks.map((link) => {
             return (
-              <Link key={link.id} href={link.link} onClick={toggleMenu}>{link.name}</Link>
-            )
+              <Link key={link.id} href={link.link} onClick={toggleMenu}>
+                {link.name}
+              </Link>
+            );
           })}
         </nav>
       </Transition>
