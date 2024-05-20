a6858ba02fb6101db6890d0e705725bb1806a66d
tricky <trickya23@gmail.com>
2024-05-20 07:02:36 +0000

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "vheeworld.org/",
+      url: "https://www.vheeworld.org/",
       priority: 1,
       changeFrequency: "yearly",
       lastModified: new Date()
     },
     {
-      url: "vheeworld.org/about",
+      url: "https://www.vheeworld.org/about",
       priority: 0.8,
       changeFrequency: "monthly",
       lastModified: new Date()
     },
     {
-      url: "vheeworld.org/projects",
+      url: "https://www.vheeworld.org/projects",
       priority: 0.8,
       changeFrequency: "monthly",
       lastModified: new Date()
     },
     {
-      url: "vheeworld.org/blog",
+      url: "https://www.vheeworld.org/blog",
       priority: 0.8,
       changeFrequency: "monthly",
       lastModified: new Date()
