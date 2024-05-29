d93dcb41372b9d029f87d21115ee7e284d63f50c
tricky <trickya23@gmail.com>
2024-05-29 12:14:36 +0000

    add socail links

--- components/footer.tsx
+++ components/footer.tsx
@@ -66,7 +66,10 @@ export function Footer() {
             ™. All Rights Reserved.
           </span>
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
-            <Link href="#" className="md:text-gray-500 hover:text-gray-900">
+            <Link
+              href="https://web.facebook.com/vheeworldfoundation"
+              className="md:text-gray-500 hover:text-gray-900"
+            >
               <svg
                 className="w-5 h-5"
                 fill="currentColor"
@@ -80,7 +83,27 @@ export function Footer() {
                 />
               </svg>
             </Link>
-            <Link href="#" className="md:text-gray-500 hover:text-gray-900">
+            <Link
+              href="https://www.linkedin.com/in/vheeworld-foundation-938238246/?trk=people_directory&originalSubdomain=gh"
+              className="md:text-gray-500 hover:text-gray-900"
+            >
+              <svg
+                className="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </Link>
+            <Link
+              href="https://www.instagram.com/vheeworld/?hl=en"
+              className="md:text-gray-500 hover:text-gray-900"
+            >
               <svg
                 className="w-5 h-5"
                 fill="currentColor"
@@ -94,7 +117,10 @@ export function Footer() {
                 />
               </svg>
             </Link>
-            <Link href="#" className="md:text-gray-500 hover:text-gray-900">
+            <Link
+              href="https://twitter.com/vheeworld"
+              className="md:text-gray-500 hover:text-gray-900"
+            >
               <svg
                 className="w-5 h-5"
                 fill="currentColor"
