72e76a7cad6810ab0f9582c8bc0bee15a902a387
tricky <trickya23@gmail.com>
2024-06-07 07:27:52 +0000

    fix

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -3,7 +3,7 @@ import DonationForm from "@/components/donationform";
 
 export default function Page() {
   return (
-    <main className="min-h-screen py-24">
+    <main className="py-24">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <DonationForm />
       </div>
