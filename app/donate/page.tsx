f50b98e5e8fc9d67c2f1561e750c858caff51e26
tricky <trickya23@gmail.com>
2024-06-07 07:24:13 +0000

    fix donation page

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -3,6 +3,10 @@ import DonationForm from "@/components/donationform";
 
 export default function Page() {
   return (
-    <DonationForm />
+    <main className="min-h-screen py-24">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <DonationForm />
+      </div>
+    </main>
   )
 }
