fe6add06dd6a12c255fabf28428ca7972b0f7a8b
tricky <trickya23@gmail.com>
2024-06-10 07:14:53 +0000

    donation page metadata

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,4 +1,44 @@
 import DonationForm from "@/components/donationform";
+import type { Metadata } from "next";
+
+const title = "Donation || VheeWorld"
+const description = "Donate to VheeWorld Foundation to help us minimize the effects of streetism on street children and their parents"
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://vheeworld.org"),
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description,
+    url: "https://vheeworld.org",
+    siteName: "VheeWorld",
+    locale: "en",
+    type: "website",
+    images: [
+      "https://res.cloudinary.com/dbgxgfsbl/image/upload/v1714659410/VHEEWORLD%20-%20WEBSITE/IMG_4322_kvkfau.jpg",
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: title,
+    description: description,
+    card: "summary",
+    site: "@site",
+    images:
+      "https://res.cloudinary.com/dbgxgfsbl/image/upload/v1714659410/VHEEWORLD%20-%20WEBSITE/IMG_4322_kvkfau.jpg",
+  },
+};
 
 
 export default function Page() {
