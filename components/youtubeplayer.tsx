3e8c0b7ac49042587c6620c602ae236251652d11
tricky <trickya23@gmail.com>
2024-07-29 16:09:07 +0000

    fix player

--- components/youtubeplayer.tsx
+++ components/youtubeplayer.tsx
@@ -3,7 +3,7 @@ export default function YoutubePlayer() {
     <section className="mt-20 px-12 md:px-24">
       <h2 className="text-3xl font-bold text-gray-800 mb-8">Social Impact</h2>
       <div>
-        <iframe title="Youtube Player" width={900} height={507} src="https://www.youtube.com/embed/1MczTdSN3GY?si=sgH0UdE-vrvgfTee" allowFullScreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" />
+        <iframe title="Youtube Player" width={560} height={315} src="https://www.youtube.com/embed/1MczTdSN3GY?si=sgH0UdE-vrvgfTee" allowFullScreen allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" />
       </div>
     </section>
   )
